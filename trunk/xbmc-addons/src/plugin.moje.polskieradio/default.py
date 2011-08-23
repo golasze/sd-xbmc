51fbe93531ee8d7f652c433f020a50b296bcba9c
plesken <plesken@375c9ea4-fdeb-a317-b137-06f34e9bcef6>
2011-08-23 11:17:29 +0000

    mojepolskieradio - poprawki

--- trunk/xbmc-addons/src/plugin.moje.polskieradio/default.py
+++ trunk/xbmc-addons/src/plugin.moje.polskieradio/default.py
@@ -10,12 +10,50 @@ scriptname = "Moje Polskie Radio"
 pradio = xbmcaddon.Addon(scriptID)
 
 
+MENU_MAIN = { 1: 'Programy',
+             2: 'Słowo',
+             3: 'Muzyka',
+             4: 'Kategorie',
+             5: 'Wszystkie kanały' }
+
 
 class MyPolishRadio:
-    def showListOptions(self):
+    def handleService(self):
         p = parser.Parser()
-        p.listChannels()
+        params = p.getParams()
+        if p.getParam(params, 'mode') == None or p.getParam(params, 'mode') == '':
+            self.LIST(MENU_MAIN)
+        elif p.getParam(params, 'mode') == '2' and p.getParam(params, 'name') == 'main':
+            p.keyLink('slowo')
+        elif p.getParam(params, 'mode') == '3' and p.getParam(params, 'name') == 'main':
+            p.keyLink('muzyka')
+        elif p.getParam(params, 'mode') == '4' and p.getParam(params, 'name') == 'main':
+            p.listCategories()
+        elif p.getParam(params, 'mode') == '4' and p.getParam(params, 'name') == 'categories':
+            if p.getParam(params, 'id') != '':
+                p.categoryLink(p.getParam(params, 'id'))
+        elif p.getParam(params, 'mode') == '5' and p.getParam(params, 'name') == 'main':
+            p.listChannels()
+
+
+    def LIST(self, table = {}):
+        for num, val in table.items():
+            self.addDir('main', num, val, False, False)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+    def addDir(self, name, mode, title, autoplay, isPlayable = True):
+        u=sys.argv[0] + "?mode=" + str(mode) + "&name=" + str(name)
+        #xbmc.log('SCIEZKA: ' + u)
+        icon = "DefaultVideoPlaylists.png"
+        if autoplay:
+            icon= "DefaultVideo.png"
+        liz=xbmcgui.ListItem(title, iconImage=icon, thumbnailImage='')
+        if autoplay and isPlayable:
+            liz.setProperty("IsPlayable", "true")
+        liz.setInfo( type="Video", infoLabels={ "Title": title } )
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u,listitem=liz, isFolder= not autoplay)
 
 
 init = MyPolishRadio()
-init.showListOptions()
+init.handleService()
--- trunk/xbmc-addons/src/plugin.moje.polskieradio/parser.py
+++ trunk/xbmc-addons/src/plugin.moje.polskieradio/parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import urllib, urllib2, re, sys, xbmcplugin, xbmcgui
-import cookielib, os, string, cookielib
+import cookielib, string
 import os, time
 import xbmcaddon, xbmcplugin, xbmcgui, xbmc
 import simplejson as json
@@ -15,12 +15,41 @@ api_url = 'http://moje.polskieradio.pl/api/'
 format = 'json'
 
 channels_url = api_url + '?key=' + idKey + '&output=' + format
+categories_url = api_url + 'categories/?key=' + idKey + '&output=' + format
 swfUrl = 'http://moje.polskieradio.pl/_swf/player.swf'
 
 
 class Parser:
     def listChannels(self):
-        raw_json = urllib.urlopen(channels_url)
+        self.createRTMP(channels_url)
+            
+
+    def listCategories(self):
+        raw_json = urllib.urlopen(categories_url)
+        content_json = raw_json.read()
+        result_object = json.loads(content_json)
+        #xbmc.log(str(result_object))
+        categoryTab = result_object["category"]
+        for i in range(len(categoryTab)):
+            id = categoryTab[i]["id"]
+            name = categoryTab[i]["name"]
+            self.addDir('categories', id, name, '')
+        #xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_NAME)
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+    
+    def keyLink(self, key):
+        url = channels_url + '&category=' + key
+        self.createRTMP(url)
+
+
+    def categoryLink(self, id):
+        url = channels_url + '&subcategory=' + id
+        self.createRTMP(url)
+
+
+    def createRTMP(self, url):
+        raw_json = urllib.urlopen(url)
         content_json = raw_json.read()
         result_object = json.loads(content_json)
         channelTab = result_object["channel"]
@@ -42,11 +71,9 @@ class Parser:
                 rtmp += ' live=true'
                 self.addLink(title, image, rtmp)
         xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_TITLE)
-        xbmcplugin.endOfDirectory(int(sys.argv[1]))
-            
-
-        
-               
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))        
+                       
+              
     def addLink(self, title, image, url):
         u = url
         #xbmc.output(u)
@@ -56,5 +83,43 @@ class Parser:
         liz.setProperty("IsPlayable", "true")
         liz.setInfo( type="Music", infoLabels={ "Title": title } )
         xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False) 
+
+
+    def addDir(self, name, id, title, icon):
+        params = self.getParams()
+        mode = self.getParam(params, "mode")
+        u=sys.argv[0] + "?mode=" + str(mode) + "&id=" + str(id) + "&name=" + str(name)
+        if icon == '':
+            icon= "DefaultVideo.png"
+        liz=xbmcgui.ListItem(title, iconImage=icon, thumbnailImage=icon)
+        liz.setProperty("IsPlayable", "false")
+        liz.setInfo( type="Music", infoLabels={ "Title": title } )
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=u,listitem=liz, isFolder=True)
             
-            
+  
+    def getParam(self, params, name):
+        try:
+            result = params[name]
+            result = urllib.unquote_plus(result)
+            return result
+        except:
+            return None 
+        
+        
+    def getParams(self):
+        param=[]
+        paramstring=sys.argv[2]
+        xbmc.log('raw param string: ' + paramstring)
+        if len(paramstring)>=2:
+            params=sys.argv[2]
+            cleanedparams=params.replace('?','')
+            if (params[len(params)-1]=='/'):
+                params=params[0:len(params)-2]
+            pairsofparams=cleanedparams.split('&')
+            param={}
+            for i in range(len(pairsofparams)):
+                splitparams={}
+                splitparams=pairsofparams[i].split('=')
+                if (len(splitparams))==2:
+                    param[splitparams[0]]=splitparams[1]
+        return param         
