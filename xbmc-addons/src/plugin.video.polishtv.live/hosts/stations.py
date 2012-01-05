f890f04a74d284a3585618f7b4adad55a643bc40
plesken <plesken@375c9ea4-fdeb-a317-b137-06f34e9bcef6>
2012-01-05 12:35:23 +0000

    Issue 35 Modyfikacja/dodanie statycznych kanałów do stations.py na podstawie linków od kwalczak85

--- xbmc-addons/src/plugin.video.polishtv.live/hosts/stations.py
+++ xbmc-addons/src/plugin.video.polishtv.live/hosts/stations.py
@@ -15,12 +15,17 @@ import pLog
 log = pLog.pLog()
 
 
-MENU_TAB = [ [ "ESKA TV", "mms://streamtv.eska.pl:6000/eskatv" ],
+MENU_TAB = [ [ "ESKA TV", "rtmp://46.105.112.212:1935/live playpath=mpegts.stream swfUrl=http://www.eska.tv/thrdparty/flowplayer/flowplayer.rtmp-3.1.4.swf pageUrl=http://www.eska.tv/player  live=true swfVfy=true" ],
              [ "Interia TV", "mms://streamlive.interia.pl/interiatv400" ],
              [ "iTVP", "mms://stream.mni.pl/ITV" ],
+             [ "Oczko tv", "rtmp://stream7.idnes.cz/live/ playpath=ocko swfUrl=http://g.idnes.cz/swf/flv/player.swf pageUrl=http://www.megawypas.pl/readarticle.php?article_id=194  live=true swfVfy=true" ],
+             [ "Rebel.tv", "rtmp://46.105.108.166:80/publishlive?play=123452 playpath=rebeltv swfUrl=http://www.popler.tv/player/flowplayer.cluster.swf pageUrl=http://www.megawypas.pl/readarticle.php?article_id=405  live=true swfVfy=true" ],
+             [ "Tele 5", "http://dcs-193-111-38-246.atmcdn.pl/streams/o2/Antel/antel.livx" ],
              [ "Trwam", "http://195.94.205.211/Trwam" ],
+             [ "TV Biznes", "http://dcs-193-111-38-248.atmcdn.pl/streams/o2/TVBiznes/TVbiznes.livx" ],
              [ "TVN", "http://89.191.147.33/3" ],
-             [ "TVN24 [chello]", "mms://stream.livetv.chello.pl/TVN24" ] ]
+             [ "TVN24 [chello]", "mms://stream.livetv.chello.pl/TVN24" ],
+             [ "TVN TURBO [BEZ REKLAM]", "rtmp://adm.live.tvtp.pl/TVNTurbo/ playpath=tv swfUrl=http://www.tvtp.pl/player/player_o_video.swf?pUrl=tvnturbo_c.e9baa14091ebd631a7a7d03445c18c18&allowfullscreen=true&pAutoplay=1&pStreaming=11 pageUrl=http://www.tvtp.pl/player/player_o_video.swf?pUrl=tvnturbo_c.e9baa14091ebd631a7a7d03445c18c18&allowfullscreen=true&pAutoplay=1&pStreaming=11  live=true swfVfy=true" ] ]
 
 
 class StreamStations:
