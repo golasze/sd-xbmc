3524af5a5e8f8cd974176f3737cb0d3d14f841eb
jatrn69 <jatrn69@gmail.com>
2012-10-26 00:51:33 +0000

    STATIONS: uaktualniona lista

--- xbmc-addons/src/plugin.video.polishtv.live/hosts/stations.py
+++ xbmc-addons/src/plugin.video.polishtv.live/hosts/stations.py
@@ -25,6 +25,7 @@ MENU_TAB = [ [ "ESKA TV", "rtmp://46.105.112.212:1935/live playpath=mpegts.strea
             [ "Czwórka Polskie Radio", "rtmp://stream85.polskieradio.pl/video/czworka.sdp" ],
             [ "Kiss TV", "rtmp://kisstelevision.es.flash3.glb.ipercast.net/kisstelevision.es-live/live" ],
             [ "CT.Fm", "rtmp://80.245.113.12/live/pubtalk2?ctfm&ctfm2012pgp" ],
+            [ "Óčko TV", "rtmp://194.79.52.79/ockoi/ockoHQ1" ],  
             [ "TVN", "rtmp://198.105.217.36/live/telewizyjka_pl_mix_?id=119553" ],
             [ "Polsat", "rtmp://75.126.203.130/stream/darmowatelewizjacompolsat?id=59168" ],
             [ "TVP INFO ", "rtmp://freeview.fms.visionip.tv/live/tvnetwork-polskaplus-tvpinfo-hsslive-25f-4x3-SDh?extsessionid=50444e8f09104-69afb3db1bb7434f243177b5996b8af1" ],
@@ -50,11 +51,12 @@ MENU_TAB = [ [ "ESKA TV", "rtmp://46.105.112.212:1935/live playpath=mpegts.strea
             [ "RTV Lubuska", "rtmp://94.23.0.87/bankier/rtl" ],
             [ "TV Asta ", "http://xeon.asta-net.pl/tvasta" ],
             [ "CW24TV", "rtmp://cdn4.stream360.pl:1935/CW24/transmisja_live" ],
-            [ "Telewizja lubuszan", "rtmp://212-104.livestream.com/mogulus-stream-edge/tllive/rtmp://212-97.livestream.com/mogulus/tllive/40c823ce-648b-44cd-9596-d5f4d6d31c8d" ],
+            [ "Tawizja", "rtmp://wwz1.4vod.tv:1935/4vod-rtp/tawizja.stream " ],
+            [ "Dla Ciebie.Tv", "rtsp://mediaserver02.artcom.pl/live/DlaCiebieTv800.stream" ],        
+            [ "Telewizja lubuszan", "rtmp://212-112.livestream.com:80/mogulus-stream-edge/tllive/rtmp://212-47.livestream.com/mogulus/tllive/1da1455e-f1ad-47bb-b019-d8a0099bd196" ],
             [ "TiWi", "rtmp://extondemand.livestream.com:80/ondemand/trans/dv02/mogulus-user-files/chv2tivinet/2009/05/11/b4ae4fc2-a938-488b-9731-cb93278a10ad" ],
             [ "Panorama Polska", "rtmp://extondemand.livestream.com/ondemand/trans/dv03/mogulus-user-files/chpanoramapolska/2010/01/07/5825602e-bdd1-490f-8680-4054c7db78ca" ],
             [ "Zdrowy Puls", "rtmp://extondemand.livestream.com/ondemand/trans/dv15/mogulus-user-files/chzdrowypuls/2012/04/04/15bbcc6c-30dc-4f08-91ca-b14dc40bbe3a" ],
-            [ "Oczko tv", "rtmp://stream7.idnes.cz/live/ocko playpath=ocko swfUrl=http://g.idnes.cz/swf/flv/player.swf pageUrl=http://www.megawypas.pl/readarticle.php?article_id=194  live=true swfVfy=true" ],
 #           [ "SKY NEWS", "mms://194.88.72.17:1755/skynews_wmlz_live300k" ],
 #           [ "CNN", "rtsp://media2.lsops.net:554/live/cnn_en_medium.sdp" ],
 #           [ "Russia Today", "rtmp://fms5.visionip.tv/live/RT_3" ],
@@ -62,10 +64,9 @@ MENU_TAB = [ [ "ESKA TV", "rtmp://46.105.112.212:1935/live playpath=mpegts.strea
 #           [ "The Voice", "http://62.41.56.32:80/PUBLIC_votv_fi" ],
 #           [ "France 24 FR ", "http://stream1.france24.yacast.net/f24_livefr" ],
 #           [ "France 24 ANG ", "http://stream1.france24.yacast.net/f24_liveen" ],
-
-             
-             
+#           [ "ČT24 ", "rtmp://wcdn42.nacevi.cz:80/CT24?id=HRydF7MABFzyrU7&publisher=lss/CT24-MP4_576p.stream" ],
 #             [ "Publika TV", "rtmp://91.230.214.56/publika/livepublika1" ],
+#            [ "Big Pond Sport ", "http://cht-cdn220-is-12.se.bptvlive.ngcdn.telstra.com/bp_online_bpsport_high" ],
              [ "TVN24 [chello]", "mms://stream.livetv.chello.pl/TVN24" ],
              [ "TVN TURBO [BEZ REKLAM]", "rtmp://adm.live.tvtp.pl/TVNTurbo/ playpath=tv swfUrl=http://www.tvtp.pl/player/player_o_video.swf?pUrl=tvnturbo_c.e9baa14091ebd631a7a7d03445c18c18&allowfullscreen=true&pAutoplay=1&pStreaming=11 pageUrl=http://www.tvtp.pl/player/player_o_video.swf?pUrl=tvnturbo_c.e9baa14091ebd631a7a7d03445c18c18&allowfullscreen=true&pAutoplay=1&pStreaming=11  live=true swfVfy=true" ] ]
 
@@ -91,4 +92,3 @@ class StreamStations:
        
     def handleService(self):
         self.showStations()
-
