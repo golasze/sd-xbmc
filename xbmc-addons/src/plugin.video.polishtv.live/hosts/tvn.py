f2a65c3fe7b870282189bcae0716d03b27d997fd
jatrn69 <jatrn69@gmail.com>
2012-10-28 00:42:04 +0000

    TVN: wylaczone zaznaczenie ogladnietego video (issue #104). Funkcja i tak nie dzialala ze wzgledu na dynamiczna nazwe plikow video.

--- xbmc-addons/src/plugin.video.polishtv.live/hosts/tvn.py
+++ xbmc-addons/src/plugin.video.polishtv.live/hosts/tvn.py
@@ -324,9 +324,10 @@ class tvn:
         return "http://redir.atmcdn.pl/http/%s?salt=%s&token=%s" % (url, salt, encryptedTokenHEX)
 
     def watched(self, videoUrl):
+        wasWatched = ''
         videoPath = videoUrl[0:1+videoUrl.rfind('/')]
-        sql_data = "SELECT count(*) FROM files LEFT JOIN path ON files.idPath = path.idPath WHERE path.strPath = '" + videoPath + "' AND files.playCount > 0"
-        xml_data = xbmc.executehttpapi( "QueryVideoDatabase(%s)" % urllib.quote_plus( sql_data ), )
+    #    sql_data = "SELECT count(*) FROM files LEFT JOIN path ON files.idPath = path.idPath WHERE path.strPath = '" + videoPath + "' AND files.playCount > 0"
+    #    xml_data = xbmc.executehttpapi( "QueryVideoDatabase(%s)" % urllib.quote_plus( sql_data ), )
         wasWatched = re.findall( "<field>(.*?)</field>", xml_data)[0]
         if wasWatched == "1":
             return True
