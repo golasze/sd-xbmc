1173d83b6194489b7eed0b294266cab24a9db771
jatrn69 <jatrn69@gmail.com>
2012-11-15 17:38:47 +0000

    pCommon.py - dodane kilka funkcji

--- xbmc-addons/src/plugin.video.polishtv.live/resources/lib/pCommon.py
+++ xbmc-addons/src/plugin.video.polishtv.live/resources/lib/pCommon.py
@@ -52,11 +52,30 @@ class common:
         data = response.read()
         response.close()
         cj.save(COOKIEFILE)
-        return data        
+        return data
+        
+    def makeABCList(self):
+        strTab = []
+        strTab.append('0 - 9');
+        for i in range(65,91):
+            strTab.append(str(unichr(i)))	
+        return strTab
+    
+    def getItemByChar(self, char, tab):
+        strTab = []
+        char = char[0]
+        for i in range(len(tab)):
+            if ord(char) >= 65:
+                if tab[i][0].upper() == char:
+                    strTab.append(tab[i])
+            else:
+                if ord(tab[i][0]) >= 48 and ord(tab[i][0]) <= 57:
+                    strTab.append(tab[i])
+        return strTab       
     
     def isNumeric(self,s):
-	try:
-	    float(s)
-	    return True
-	except ValueError:
-	    return False
+        try:
+            float(s)
+            return True
+         except ValueError:
+            return False
