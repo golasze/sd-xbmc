d7fc041102e5bffe6aa4f1c35ad06905763485cd
TomashJ29 <TomashJ29@gmail.com>
2012-12-28 11:29:17 +0000

    default: dodano kategorię rozrywka (narazie ukryta), Kabarety: poprawki

--- xbmc-addons/src/plugin.video.polishtv.live/hosts/kabarety.py
+++ xbmc-addons/src/plugin.video.polishtv.live/hosts/kabarety.py
@@ -11,9 +11,11 @@ ptv = xbmcaddon.Addon(scriptID)
 BASE_RESOURCE_PATH = os.path.join( ptv.getAddonInfo('path'), "../resources" )
 sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib" ) )
 
-import pLog, settings, Parser, urlparser, pCommon
+import pLog, settings, Parser, urlparser, pCommon, Errors, traceback
 
 log = pLog.pLog()
+
+dbg = ptv.getSetting('default_debug')
 
 HOST = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.18) Gecko/20110621 Mandriva Linux/1.9.2.18-0.1mdv2010.2 (2010.2) Firefox/3.6.18'
 SERVICE = 'kabarety'
@@ -31,6 +33,28 @@ SERVICE_MENU_TABLE = {1: "Najnowsze",
 		      5: "Historia Wyszukiwania"
 		      }
 
+CHARS = [
+    [ '&#261;', 'ą' ],
+    [ '&#263;', 'ć' ],
+    [ '&#281;', 'ę' ],
+    [ '&#322;', 'ł' ],
+    [ '&#324;', 'ń' ],
+    [ '&#243;', 'ó' ],
+    [ '&#347;', 'ś' ],
+    [ '&#378;', 'ź' ],
+    [ '&#380;', 'ż' ],
+    [ '&#260;', 'Ą' ],
+    [ '&#262;', 'Ć' ],
+    [ '&#280;', 'Ę' ],
+    [ '&#321;', 'Ł' ],
+    [ '&#323;', 'Ń' ],
+    [ '&#211;', 'Ó' ],
+    [ '&#346;', 'Ś' ],
+    [ '&#377;', 'Ż' ],
+    [ '&#379;', 'Ż' ],
+    [ '&amp;', '&' ],
+]
+
 class Kabarety:
     def __init__(self):
         log.info('Loading ' + SERVICE)
@@ -39,7 +63,9 @@ class Kabarety:
         self.up = urlparser.urlparser()
         self.cm = pCommon.common()
 	self.history = pCommon.history()
-
+	self.chars = pCommon.Chars()
+	self.exception = Errors.Exception()
+	self.dir = pCommon.common()
 
     def setTable(self):
 	return SERVICE_MENU_TABLE
@@ -54,16 +80,24 @@ class Kabarety:
     def getCategoryTab(self,url):   
         strTab = []
         valTab = []
-        data = self.cm.requestData(url)
-        match = re.compile('<a href="(.+?)">(.+?)</a><br>').findall(data)
+        query_data = { 'url': url, 'use_host': True, 'host': HOST, 'use_cookie': False, 'use_post': False, 'return_data': True }
+        try:
+		data = self.cm.getURLRequestData(query_data)
+	except Exception, exception:
+		traceback.print_exc()
+		self.exception.getError(str(exception))
+		exit()
+        match = re.compile('<b>Kategorie</a><br><br>(.+?)<br><br>&nbsp;', re.DOTALL).findall(data)
         if len(match) > 0:
-	    for i in range(len(match)):
-		value = match[i]
-		strTab.append(value[0])
-		strTab.append(value[1])	
-		valTab.append(strTab)
-		strTab = []
-	    valTab.sort(key = lambda x: x[1])
+            match2 = re.compile('href="(.+?)">(.+?)</a>').findall(match[0]) 
+            if len(match2) > 0:
+                for i in range(len(match2)):
+                    value = match2[i]
+                    strTab.append(value[0])
+                    strTab.append(value[1])	
+                    valTab.append(strTab)
+                    strTab = []
+                valTab.sort(key = lambda x: x[1])
         return valTab
 
 
@@ -71,16 +105,32 @@ class Kabarety:
         table = self.getCategoryTab(url)
         for i in range(len(table)):
 	    value = table[i]
-	    title = value[1]
+	    title = self.replaceChars(value[1])
 	    if title > 0:
-		self.addDir(SERVICE, value[0], category, title, '', '', '', True, False)
+		self.addDir(SERVICE, value[0], category, title, 'cat', '', '', True, False)
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+    def replaceChars(self, string):
+        out = string
+        for i in range(len(CHARS)):
+            out = string.replace(CHARS[i][0], CHARS[i][1])
+            string = out
+        return out
+    
+    def setCHARS(self):
+        return CHARS
 
 
     def getFilmTab(self, url):
         strTab = []
         valTab = []
-        data = self.cm.requestData(url)
+        query_data = { 'url': url, 'use_host': True, 'host': HOST, 'use_cookie': False, 'use_post': False, 'return_data': True }
+        try:
+		data = self.cm.getURLRequestData(query_data)
+	except Exception, exception:
+		traceback.print_exc()
+		self.exception.getError(str(exception))
+		exit()
         link = data.replace('\n', '')
 	match = re.compile('<a class="video-mini" title="(.+?)" href=".+?">.+?<span class="duration".+?<img class="video-mini-img".+?src="http://i.ytimg.com/vi/(.+?)/0.jpg" />').findall(link)
         if len(match) > 0:
@@ -102,7 +152,7 @@ class Kabarety:
         table = self.getFilmTab(url)
         for i in range(len(table)):
 	    value = table[i]
-	    title = value[0]
+	    title = self.replaceChars(value[0])
 	    img = IMGURL + value[1] + '/0.jpg'
 	    if value[0] != 'Następna strona':
 		self.addDir(SERVICE, 'playSelectedMovie', '', title, '', value[1], img, True, False)
@@ -121,7 +171,7 @@ class Kabarety:
     def getSearchTable(self, table):
         for i in range(len(table)):
 	    value = table[i]
-	    title = value[0]
+	    title = self.replaceChars(value[0])
 	    img = IMGURL + value[1] + '/0.jpg'
 	    self.addDir(SERVICE, 'playSelectedMovie', '', title, '', value[1], img, True, False)
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
@@ -136,11 +186,22 @@ class Kabarety:
 	    self.history.addHistoryItem(SERVICE, text)
         return text
 
+    def getVideo(self,page):
+        videoUrl = []   
+        videoUrl = 'plugin://plugin.video.youtube?path=/root/video&action=play_video&videoid=' + page
+        return videoUrl
+
 
     def searchTab(self, text):
         strTab = []
         valTab = []
-        data = self.cm.requestData(MAINURL + '/search?q=' + text)
+        query_data = { 'url': MAINURL + '/search?q=' + text, 'use_host': True, 'host': HOST, 'use_cookie': False, 'use_post': False, 'return_data': True }
+        try:
+		data = self.cm.getURLRequestData(query_data)
+	except Exception, exception:
+		traceback.print_exc()
+		self.exception.getError(str(exception))
+		exit()
         link = data.replace('\n', '')
 	match = re.compile('<a class="video-mini" title="(.+?)" href=".+?">.+?<span class="duration".+?<img class="video-mini-img".+?src="http://i.ytimg.com/vi/(.+?)/0.jpg" />').findall(link)
         if len(match) > 0:
@@ -167,15 +228,16 @@ class Kabarety:
         xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=folder)
     
       
-    def LOAD_AND_PLAY_VIDEO(self, videoId):
+    def LOAD_AND_PLAY_VIDEO(self, page):
+        videoUrl = self.getVideo(page)
         ok=True
-        if videoId == '':
+        if videoUrl == '':
             d = xbmcgui.Dialog()
             d.ok('Nie znaleziono streamingu.', 'Może to chwilowa awaria.', 'Spróbuj ponownie za jakiś czas')
             return False
         try:
-            url = 'plugin://plugin.video.youtube?path=/root/video&action=play_video&videoid=' + videoId
-            xbmc.executebuiltin("xbmc.PlayMedia("+url+")")
+            xbmcPlayer = xbmc.Player()
+            xbmcPlayer.play(videoUrl)
         except:
             d = xbmcgui.Dialog()
             d.ok('Błąd przy przetwarzaniu, lub wyczerpany limit czasowy oglądania.', 'Zarejestruj się i opłać abonament.', 'Aby oglądać za darmo spróbuj ponownie za jakiś czas')        
@@ -190,27 +252,25 @@ class Kabarety:
         page = self.parser.getParam(params, "page")
         icon = self.parser.getParam(params, "icon")
 
-	log.info ('name: ' + str(name))
-	log.info ('title: ' + str(title))
-	log.info ('category: ' + str(category))
-	log.info ('page: ' + str(page))
+        if dbg == 'true':
+                log.info ('name: ' + str(name))
+                log.info ('title: ' + str(title))
+                log.info ('category: ' + str(category))
+                log.info ('page: ' + str(page))
 
-	if page=='': page = 1
+	if str(page)=='None' or page=='': page = 1
 		
         if name == None:
             self.listsMainMenu(SERVICE_MENU_TABLE)	
    
 	elif category == self.setTable()[1]:
-            name = 'nic'
-	    self.getFilmTable(NOW_LINK + str(page), page, category, name)	    
+	    self.getFilmTable(NOW_LINK + str(page), page, category, 'name')	    
 
 	elif category == self.setTable()[2]:
-            name = 'nic'
-	    self.getFilmTable(NAJ_LINK + str(page), page, category, name)	
+	    self.getFilmTable(NAJ_LINK + str(page), page, category, 'name')	
 
-	elif category == self.setTable()[3]:
-            categoryfix = category + 'fix'
-	    self.listsCategoriesMenu(MAINURL, categoryfix)
+	elif category == self.setTable()[3] and name == 'main-menu':
+	    self.listsCategoriesMenu(MAINURL, category)
 
 	elif category == self.setTable()[4]:
 	    text = self.searchInputText()
@@ -221,10 +281,9 @@ class Kabarety:
 	if category == 'history' and name != 'playSelectedMovie':
 	    self.getSearchTable(self.searchTab(name))	
 	
-	if category == 'Kategoriefix':
+	if category == self.setTable()[3] and name != 'main-menu':
             url = name + '/page:' + str(page)
             self.getFilmTable(url, page, category, name)	    
 	    	    
         if name == 'playSelectedMovie':
             self.LOAD_AND_PLAY_VIDEO(page)
-      
