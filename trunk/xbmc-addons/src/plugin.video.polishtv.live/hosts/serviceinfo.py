ca7da6edb2f6bf7be02a9bb1d210118323812417
plesken <plesken@375c9ea4-fdeb-a317-b137-06f34e9bcef6>
2013-01-10 19:21:29 +0000

    Dodanie ikonek i metod osobnego wyświetlania w serviceinfo wszystkie/online/offline/workaround

--- trunk/xbmc-addons/src/plugin.video.polishtv.live/hosts/serviceinfo.py
+++ trunk/xbmc-addons/src/plugin.video.polishtv.live/hosts/serviceinfo.py
@@ -19,6 +19,10 @@ dbg = ptv.getSetting('default_debug')
 STATUSURL = 'http://sd-xbmc.org/xbmc/servicestatus.xml'
 #get actioncodes from keymap.xml
 
+ACTION_ALL = 111
+ACTION_ONLINE = 112
+ACTION_WORK = 113
+ACTION_OFFLINE = 114
 ACTION_EXIT = 117
 ACTION_LIST = 120
 
@@ -83,33 +87,75 @@ class WindowServiceInfo(xbmcgui.WindowXMLDialog):
         #self.list = {}      
        
     def onInit(self):
-        list = self.getControl(ACTION_LIST)
         if dbg == 'true':
             log.info('ServiceInfo - WindowServiceInfo()[onInit] -> table: ' + str(self.sTab))
-        for i in range(len(self.sTab)):
-            nameS = self.sTab[i][0]
-            statusS = self.sTab[i][1]
-            dateS = self.sTab[i][2]
-            descS = self.sTab[i][3]
-            statusR = '[I][COLOR green]' + statusS + '[/COLOR][/I]'
-            descR = '[I]' + descS + '[/I]'
-            if statusS == 'OFFLINE':
-                statusR = '[I][COLOR red]' + statusS + '[/COLOR][/I]'
-            elif statusS == 'WORKAROUND':
-                statusR = '[I][COLOR yellow]' + statusS + '[/COLOR][/I]'
-            info = "%s (%s, %s)" % ('[B][COLOR blue]' + nameS + '[/COLOR][/B]', statusR, dateS)
-            l = xbmcgui.ListItem(label = info, label2 = descR)
-            list.addItem(l)
+        self.generateListItem(ACTION_ALL)
     
     def onAction(self, action):
         if action == 1001:
             self.close()
+        elif action == 1002:
+            self.generateListItem(ACTION_ALL)
+        elif action == 1003:
+            self.generateListItem(ACTION_WORK)
+        elif action == 1004:
+            self.generateListItem(ACTION_OFFLINE)
+        elif action == 1005:
+            self.generateListItem(ACTION_ONLINE)
+        
     
     def onClick(self, controlID):
         if controlID == ACTION_EXIT:
             self.onAction(1001)
+        elif controlID == ACTION_ALL:
+            self.onAction(1002)
+        elif controlID == ACTION_WORK:
+            self.onAction(1003)
+        elif controlID == ACTION_OFFLINE:
+            self.onAction(1004)
+        elif controlID == ACTION_ONLINE:
+            self.onAction(1005)
     
     def onFocus(self, controlID):
         pass
      
-    
+    def generateListItem(self, id):
+        if dbg == 'true':
+            log.info('ServiceInfo - WindowServiceInfo()[generateListItem] -> action id: ' + str(id))
+        list = self.getControl(ACTION_LIST)
+        list.reset()
+        if len(self.sTab) > 0:
+            for i in range(len(self.sTab)):
+                nameS = self.sTab[i][0]
+                statusS = self.sTab[i][1]
+                dateS = self.sTab[i][2]
+                descS = self.sTab[i][3]
+                descR = '[I]' + descS + '[/I]'
+                if id == ACTION_ALL:
+                    statusR = '[I][COLOR green]' + statusS + '[/COLOR][/I]'
+                    descR = '[I]' + descS + '[/I]'
+                    if statusS == 'OFFLINE':
+                        statusR = '[I][COLOR red]' + statusS + '[/COLOR][/I]'
+                    elif statusS == 'WORKAROUND':
+                        statusR = '[I][COLOR yellow]' + statusS + '[/COLOR][/I]'
+                    info = "%s (%s, %s)" % ('[B][COLOR blue]' + nameS + '[/COLOR][/B]', statusR, dateS)
+                    l = xbmcgui.ListItem(label = info, label2 = descR)
+                    list.addItem(l)    
+                elif id == ACTION_WORK:
+                    if statusS == 'WORKAROUND':
+                        statusR = '[I][COLOR yellow]' + statusS + '[/COLOR][/I]'
+                        info = "%s (%s, %s)" % ('[B][COLOR blue]' + nameS + '[/COLOR][/B]', statusR, dateS)
+                        l = xbmcgui.ListItem(label = info, label2 = descR)
+                        list.addItem(l)
+                elif id == ACTION_OFFLINE:
+                    if statusS == 'OFFLINE':
+                        statusR = '[I][COLOR red]' + statusS + '[/COLOR][/I]'
+                        info = "%s (%s, %s)" % ('[B][COLOR blue]' + nameS + '[/COLOR][/B]', statusR, dateS)
+                        l = xbmcgui.ListItem(label = info, label2 = descR)
+                        list.addItem(l)
+                elif id == ACTION_ONLINE:
+                    if statusS == 'ONLINE':
+                        statusR = '[I][COLOR green]' + statusS + '[/COLOR][/I]'
+                        info = "%s (%s, %s)" % ('[B][COLOR blue]' + nameS + '[/COLOR][/B]', statusR, dateS)
+                        l = xbmcgui.ListItem(label = info, label2 = descR)
+                        list.addItem(l)                    
