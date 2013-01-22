31ebb790e5bb0d70d7279bef980a01214eb52a71
jatrn69@gmail.com <jatrn69@gmail.com@375c9ea4-fdeb-a317-b137-06f34e9bcef6>
2013-01-22 17:59:45 +0000

    KABARETY: usuniecie nieuzywanej biblioteki

--- trunk/xbmc-addons/src/plugin.video.polishtv.live/hosts/kabarety.py
+++ trunk/xbmc-addons/src/plugin.video.polishtv.live/hosts/kabarety.py
@@ -11,7 +11,7 @@ ptv = xbmcaddon.Addon(scriptID)
 BASE_RESOURCE_PATH = os.path.join( ptv.getAddonInfo('path'), "../resources" )
 sys.path.append( os.path.join( BASE_RESOURCE_PATH, "lib" ) )
 
-import pLog, settings, Parser, urlparser, pCommon, Errors, traceback
+import pLog, settings, Parser, pCommon, Errors, traceback
 
 log = pLog.pLog()
 
@@ -61,12 +61,11 @@ class Kabarety:
         log.info('Loading ' + SERVICE)
         self.settings = settings.TVSettings()
         self.parser = Parser.Parser()
-        self.up = urlparser.urlparser()
         self.cm = pCommon.common()
 	self.history = pCommon.history()
 	self.chars = pCommon.Chars()
 	self.exception = Errors.Exception()
-	self.dir = pCommon.common()
+
 
     def setTable(self):
 	return SERVICE_MENU_TABLE
